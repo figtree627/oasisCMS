fd4ad2d9d9ecc2c97736d006c63d895a06e0a21e
figtree <figtree@DESKTOP-L99IO2P>
2024-03-22 11:36:10 +0900

    oasisCMS 최초커밋

---
+++ oasisCMS/src/main/java/com/application/oasisCMS/OasisCmsApplication.java
@@ -0,0 +1,13 @@
+package com.application.oasisCMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OasisCmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OasisCmsApplication.class, args);
+	}
+
+}
